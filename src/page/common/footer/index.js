dc9b8b216cf8376a86996bfc848880405b2e2099
Zihao Tao <taozihao@uw.edu>
2019-06-04 00:04:06 -0700

    dark mode

---
+++ src/page/common/footer/index.js
@@ -0,0 +1,12 @@
+/*
+* @Author: Zihao Tao
+* @Date:   2019-06-04 00:03:07
+* @Last Modified by:   Zihao Tao
+* @Last Modified time: 2019-06-04 00:03:56
+*/
+'use strict';
+require('./index.css');
+
+(function() {
+  $('.footer').width($('.w').with());
+})();
