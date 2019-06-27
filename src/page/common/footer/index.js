95fea45030d51419203ddbaf216e12fe5c94e6c7
Zihao Tao <taozihao@uw.edu>
2019-06-27 13:49:07 -0700

    s

--- src/page/common/footer/index.js
+++ src/page/common/footer/index.js
@@ -2,11 +2,11 @@
 * @Author: Zihao Tao
 * @Date:   2019-06-04 00:03:07
 * @Last Modified by:   Zihao Tao
-* @Last Modified time: 2019-06-04 00:12:52
+* @Last Modified time: 2019-06-27 13:42:46
 */
 'use strict';
 require('./index.css');
-
+var $ = require('jquery');
 (function() {
   $('.footer').width($('.w').width());
 })();
