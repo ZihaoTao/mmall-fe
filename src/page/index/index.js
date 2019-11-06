d21e69361964c273ada44edcfb11735a145497ad
Zihao Tao <taozihao@uw.edu>
2019-11-05 19:33:48 -0800

    sd

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -20,28 +20,23 @@ $(function() {
     $('.banner-con').html(bannerHtml);
     // initilaize banner
     var $slider = $('.banner').unslider({
-        speed: 500,               //  The speed to animate each slide (in milliseconds)
-        delay: 3000,              //  The delay between slide animations (in milliseconds)
-        complete: function() {},  //  A function that gets called after every slide animation
+        speed: 1000,               //  The speed to animate each slide (in milliseconds)
+        delay: 3500,              //  The delay between slide animations (in milliseconds)
         keys: true,               //  Enable keyboard (left, right) arrow shortcuts
         dots: false,               //  Display dot navigation
-        fluid: false              //  Support responsive design. May break non-responsive designs
+        fluid: true              //  Support responsive design. May break non-responsive designs
     });
-    //event listener
-    $('.banner-con .banner-arrow').click(function() {
-        var forward = $(this).hasClass('prev') ? 'prev' : 'next';
-        $slider.data('unslider')[forward]();
-    });
+    // //event listener
+    // $('.banner-con .banner-arrow').click(function() {
+    //     var forward = $(this).hasClass('prev') ? 'prev' : 'next';
+    //     $slider.data('unslider')[forward]();
+    // });
 
     $('.floor-wrap .floor-list .floor-item').hover(function() {
         $('.floor-wrap .floor-list .floor-item').toggleClass('item_hover');
         $(this).toggleClass('item_hover');
     });
-    $('.banner-con').height($(window).height());
-    if($('body').width() > 1000) {
-        $('.home-title').height($('.home-title').width() / 3.5);
-        $('.banner-img').css({'height':$(window).height()});
-    } else {
-        $('.home-title').height($('.home-title').width() / 1.9);
-    }
+    
+    $('.home-title').height($('.home-title').width() / 3.5);
+      
 });
